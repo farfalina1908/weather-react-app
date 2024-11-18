eb252f7a30f6d3b87d9c92511d718ddcee137fb0
Elena Berezina <farfalina1908@gmail.com>
2024-11-18 15:44:23 +0200

    add Weather component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
+     <div className="container">
       <header className="App-header">
+
+      
         
         <h1>
          Weather react app
         </h1>
-       
+      <Weather/>
       </header>
       <footer>
       This project was coded by Olena Berezina and is open-sourced on  {" "}  
@@ -24,13 +28,14 @@ export default function App() {
         {" "}  and hosted on  {" "}  
         <a
           className="App-link"
-          href="https://github.com/farfalina1908/weather-react-app"
+          href="https://enchanting-dango-f50e89.netlify.app"
           target="_blank"
           rel="noopener noreferrer"
         >
          Netlify 
         </a>
       </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Weather.css"
+
+export default function Weather () {
+    return <div className="Weather">
+        <form>
+        <div className="row">
+             <div className="col-9">
+                 <input type="search" placeholder="Enter a city..." className="form-control"/>
+             </div>
+            <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary"/>
+            </div>
+           
+        </div>
+           
+        </form>
+        <h1>New York</h1>
+        <ul>
+            <li>Wenesday 7:00</li>
+            <li>Mostly Cloudy</li>
+        </ul>
+        <div className="row">
+        <div className="col-6">
+            <img src="" alt="Mostly Cloudy"/>
+             6C
+        </div>
+        <div className="col-6">
+           
+<ul>
+            <li>Precipitation: 15%</li>
+            <li>Humiditi: 72%</li>
+            <li>Wind: 13 km/h</li>
+        </ul>
+        </div>
+        </div>
+    </div> ;
+}

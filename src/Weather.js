28435f8355d4f1e9d598f95e218341f0acd01629
Elena Berezina <farfalina1908@gmail.com>
2024-11-21 15:29:19 +0200

    Used correct icon Url

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather (props) {
         temperature: response.data.main.temp,
         humidity: response.data.main.humidity,
         date: new Date(response.data.dt*1000),
-        iconUrl: ``,
+        iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         description: response.data.weather[0].description,
         wind: response.data.wind.speed,
         city: response.data.name
